dc694634b857ddc2630ed3e97ee0e80e216fbde9
Cliffield <bauerfelix@web.de>
2016-07-26 01:01:53 +0200

    create output folder if not exists

--- createheatmaps.py
+++ createheatmaps.py
@@ -3,11 +3,14 @@ from jinja2 import Environment, FileSystemLoader
 import sqlite3
 import pandas as pd
 
+# define output folder
+output_folder = "maps"
+
 # define database
-db = ""
+database = ""
 
 # connect to database
-con = sqlite3.connect(db)
+con = sqlite3.connect(database)
 
 # get table pokemon
 df = pd.read_sql_query("SELECT * from pokemon", con)
@@ -15,23 +18,30 @@ df = pd.read_sql_query("SELECT * from pokemon", con)
 # close connection to database
 con.close()
 
-# get eoncountered pokemon_id's
+# get eoncountered pokemon_ids
 found_pokemon = df.drop_duplicates(subset = "pokemon_id")
 
 # sort pokemon_id ascending
 found_pokemon_sorted = found_pokemon.sort_values(by = "pokemon_id")
 
-found_pokemon_sortet_list = found_pokemon_sorted['pokemon_id'].values.tolist()
+found_pokemon_sortet_list = found_pokemon_sorted["pokemon_id"].values.tolist()
+
+# test if folder maps exist, else create it
+try: 
+    os.makedirs(output_folder)
+except OSError:
+    if not os.path.isdir(output_folder):
+        raise
 
 # create heatmaps (html-files) for eoncoutered pokemons 
 for x in found_pokemon_sortet_list:
 	
 	pokeid = x
 	
-	df_pokeid = df[(df['pokemon_id'] == pokeid)]
+	df_pokeid = df[(df["pokemon_id"] == pokeid)]
 	
 	# get weight (times of spawns per pokemon and spawnpoint)
-	df_pokeid['weight'] = df_pokeid.groupby('spawnpoint_id')['spawnpoint_id'].transform(len)
+	df_pokeid["weight"] = df_pokeid.groupby("spawnpoint_id")["spawnpoint_id"].transform(len)
 	
 	# delete spawnpoint duplets (necesarry? )
 	df_pokeid = df_pokeid.drop_duplicates(subset = "spawnpoint_id")
@@ -44,21 +54,21 @@ for x in found_pokemon_sortet_list:
 	
 	TEMPLATE_ENVIRONMENT = Environment(
 		autoescape=False,
-		loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+		loader=FileSystemLoader(os.path.join(PATH, "templates")),
 		trim_blocks=False)
 	
 	def render_template(template_filename, context):
 		return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 	
 	def create_maps_html():
-		fname = "maps/map_" + str(pokeid) + ".html"
+		fname = output_folder + "/map_" + str(pokeid) + ".html"
 		LatLngWei = xyz
 		context = {
- 		'LatLngWei': LatLngWei,
+ 		"LatLngWei": LatLngWei,
 		}
 		#
-		with open(fname, 'w') as f:
-			html = render_template('maps.html', context)
+		with open(fname, "w") as f:
+			html = render_template("maps.html", context)
 			f.write(html)
 	
 	def main():
